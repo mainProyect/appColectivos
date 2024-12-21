c30016b5b0450407caceca43962bbb1f04c085ce
mainProyect <miguelsalas19941@gmail.com>
2024-12-21 14:08:34 -0300

    asdqwe

--- main.js
+++ main.js
@@ -1,34 +1,56 @@
-const paises = [
-    "Argentina",
-    "Paraguay",
-    "Bolivia",
-    "Brasil",
-    "Chile",
-    "uruguay"
-]
-const inputBusqueda = document.getElementById("inputBusqueda");
-const listaSugerencia = document.getElementById("listaSugerencia");
-inputBusqueda.addEventListener("input", ()=>{
-    const texto = inputBusqueda.value.trim().toLowerCase();
-    listaSugerencia.innerHTML="";
-    if (texto!==""){
-        const coincidencias = paises.filter(pais => pais.toLocaleLowerCase().includes(texto));
-        coincidencias.forEach(pais => {
-            const div = document.createElement("div")
-            div.textContent=pais
-            div.addEventListener("click", ()=>{
-                inputBusqueda.value= pais;
-                listaSugerencia.innerHTML="";
-            });
-            listaSugerencia.appendChild(div);
+document.addEventListener("DOMContentLoaded", ()=>{
+    let map;
+    let marker;
+    function initMap(lat = -34.6037, lng=-58.3816){
+        const divMapa = document.getElementById("contenedorMapa");
+        const centro = {lat,lng};
+        map = new google.maps.Map(divMapa, {
+            zoom: 14,
+            center: centro,
         });
+        marker = new google.maps.Marker({
+            position : center,
+            map : map,
+            tittle : "Por aca va el bondi"
+        })
     }
-});
-document.addEventListener("click", (e)=>{
-    if (e.target !== inputBusqueda){
+
+    function actPosicion(lat,lng){
+        const nuevaPosicion = {lat,lng}
+        marker.setPosition(nuevaPosicion)
+        map.setCenter(nuevaPosicion)
+    }
+    initMap()
+    function obtenerCoordenadas(direccion){
+
+        const url = `https://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=${encodeURIComponent(direccion)}`;
+        fetch(url)
+            .then(response=>response.json())
+            .then(data=>{
+                if (data.resultado && data.resultado.length>0){
+                    const {lat, lon}= data.resultado[0]
+                    actPosicion(lat, lon);
+                    }else{
+                        alert("No se encontraron resultados para la direccion")
+                    }
+                })
+                .cath(error => console.error("Error al obtener coordenadas: ", error));
+            }
+
+
+
+    const inputBusqueda = document.getElementById("inputBusqueda");
+    const listaSugerencia = document.getElementById("listaSugerencia");
+    inputBusqueda.addEventListener("input", ()=>{
+        const texto = inputBusqueda.value.trim().toLowerCase();
         listaSugerencia.innerHTML="";
-    }
+        if (texto!==""){
+            obtenerCoordenadas(texto)
+        }
+    })
 });
+
+
 
 
 
